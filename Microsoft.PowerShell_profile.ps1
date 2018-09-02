98d4394af2b1864eb910c7994006cfd1a3289c4d
Luiz Strobelt <lhsperez@gmail.com>
2018-09-02 01:09:07 -0300

    Add MenuCompletion and Z

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,9 @@
 Import-Module posh-git
 Import-Module oh-my-posh
 Import-Module DockerCompletion
+Import-Module PSReadLine
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Import-Module z
 Set-Theme Agnoster
 
 function make-link ($target, $link) {
